1149d25b7c52edeb2c5e7814331f87d91b037d7c
art10 <art10@192.168.219.125>
2020-10-01 18:04:05 +0900

    update Calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,6 +3,11 @@ package calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Calendar");
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		System.out.println("--------------------------------------------------");
+		System.out.println("1\t2\t3\t4\t5\t6\t7");
+		System.out.println("8\t9\t10\t11\t12\t13\t14");
+		System.out.println("15\t16\t17\t18\t19\t20\t21");
+		System.out.println("22\t23\t24\t25\t26\t27\t28");
 	}
 }
